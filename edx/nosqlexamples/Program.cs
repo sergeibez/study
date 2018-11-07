d38a664fb7adf0a0daecf36608d8d7b840855f69
Sergey Bezvershenko <sergey@albatros.net>
2018-11-07 14:32:47 +0100

    create the test project for work with Azure

---
+++ edx/nosqlexamples/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Table;
+
+namespace nosqlexamples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GetStorageData().Wait();
+        }
+
+        public static async Task GetStorageData() 
+        {
+            var account = CloudStorageAccount.Parse("");
+
+            var client = account.CreateCloudTableClient();
+
+            var table = client.GetTableReference("people");
+
+            await table.CreateIfNotExistsAsync();
+        }
+    }
+}
